78535ba1244b7de13200ab25b8a4a9a2116d845b
Devnatavaz <contato.natavaz@gmail.com>
2025-04-03 14:20:40 -0300

    atualização

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-app-back-f5y8.onrender.com")
+    websocket = new WebSocket("wss://chat-app-back-f5y8.onrender.com")
     websocket.onmessage = processMessage
 }
 
